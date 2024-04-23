edb19a59941f8fda2cbc22865ca982781df7fd08
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-04-22 21:31:44 -0400

    added time functionality

--- script.js
+++ script.js
@@ -7,19 +7,46 @@
     - pet can evolve at level 5
 - ???
 */
+let hungerText = document.getElementById('hungerText');
+let sleepyText = document.getElementById('sleepyText');
+let boredomText = document.getElementById('boredomText');
+let nameTxt = document.getElementById('nameText');
+let minutes = 0;
+let seconds = 0;
+time();
 
 // Create a pet class with name/level/hunger/boredom/sleepiness
-
 class Tamagotchi {
-    constructor(name) {
-        this.name = name;
-        this.age = 0;
-        this.hunger = 0;
-        this.boredom = 0;
-        this.sleepiness = 0;
-    }
-    ageUp() {
-        // conditional statement to see if time is at a 00:30 interval (adjust increment for testing)
-    }
+  constructor(name) {
+    this.name = name;
+    this.level = 0;
+    this.hunger = 0;
+    this.boredom = 0;
+    this.sleepiness = 0;
+  }
+  levelUp() {
+    // conditional statement to see if time is at a 00:30 interval (adjust increment for testing)
+    // let lvlText = document.getElementById('lvlText');
+    // if (seconds === 30) {
+    //     level += 1
+    //     lvlText.innerHTML = this.level;
+    // }
+  }
 }
 
+function time() {
+    setInterval(function () {
+      if (seconds < 10 && minutes < 10) {
+        document.getElementById("clock").innerHTML =
+          "0" + minutes + ":0" + seconds;
+      } else {
+        document.getElementById("clock").innerHTML =
+          "0" + minutes + ":" + seconds;
+      }
+      seconds++;
+      if (seconds === 60) {
+        seconds = 0;
+        minutes += 1;
+      }
+    }, 1000);
+  }
