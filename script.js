bc2d3e1d5531199abaa3f717ad2db65cfed9249a
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-04-22 20:24:09 -0400

    basic UI structure/styling

---
+++ script.js
@@ -0,0 +1,25 @@
+/* 
+- create a class to hold game/pet information and methods
+- create functional buttons to control feed/play/lights
+- create multiple values for hunger/boredom/sleepiness/level
+- create a clock (hh:mm) where minutes pass in seconds
+- have our pet values change with time
+    - pet can evolve at level 5
+- ???
+*/
+
+// Create a pet class with name/level/hunger/boredom/sleepiness
+
+class Tamagotchi {
+    constructor(name) {
+        this.name = name;
+        this.age = 0;
+        this.hunger = 0;
+        this.boredom = 0;
+        this.sleepiness = 0;
+    }
+    ageUp() {
+        // conditional statement to see if time is at a 00:30 interval (adjust increment for testing)
+    }
+}
+
